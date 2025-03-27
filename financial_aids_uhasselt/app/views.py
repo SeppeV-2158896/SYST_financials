5c94cc7cc1264f34ed5a6a1d235529d430f1ea70
SeppeV-2158896 <seppe.vandenberk@student.uhasselt.be>
2025-03-27 20:10:29 +0100

    route through button login

--- financial_aids_uhasselt/app/views.py
+++ financial_aids_uhasselt/app/views.py
@@ -9,5 +9,25 @@ def index(request):
     return HttpResponse(template.render({}, request))
 
 def simulate(request):
-    questions = Question.objects.all()  # Fetch all questions from the database
-    return render(request, 'account_info.html', {'questions': questions})
+    # If user is not authenticated and they are not a guest
+    if not request.user.is_authenticated:
+        if 'guest' in request.GET:
+            # Treat the user as a guest
+            username = None
+        else:
+            # If they are not logged in and not a guest, redirect them to login page
+            return index()
+    else:
+        # If the user is authenticated, use their username
+        username = request.user.username
+    
+    # Get all questions from the database
+    questions = Question.objects.all()
+    
+    # Pass the user status and questions to the template
+    context = {
+        'questions': questions,
+        'username': username or 'Guest',  # If username is None, display 'Guest'
+    }
+
+    return render(request, 'simulate.html', context)
