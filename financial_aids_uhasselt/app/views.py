30f51c20f81a085f616348e38226cbada6548b4c
SeppeV-2158896 <125284904+SeppeV-2158896@users.noreply.github.com>
2025-03-27 21:22:42 +0100

    fix reroute

--- financial_aids_uhasselt/app/views.py
+++ financial_aids_uhasselt/app/views.py
@@ -9,14 +9,17 @@ def index(request):
     return HttpResponse(template.render({}, request))
 
 def simulate(request):
+    # authenticated = request.user.is_authenticated
+    authenticated = True  # For testing purposes, assume user is authenticated
+    
     # If user is not authenticated and they are not a guest
-    if not request.user.is_authenticated:
+    if not authenticated:
         if 'guest' in request.GET:
             # Treat the user as a guest
             username = None
         else:
             # If they are not logged in and not a guest, redirect them to login page
-            return index()
+            return index(request)
     else:
         # If the user is authenticated, use their username
         username = request.user.username
@@ -28,6 +31,7 @@ def simulate(request):
     context = {
         'questions': questions,
         'username': username or 'Guest',  # If username is None, display 'Guest'
+        'is_authenticated': True,
     }
 
-    return render(request, 'simulate.html', context)
+    return render(request, 'account_info.html', context)
