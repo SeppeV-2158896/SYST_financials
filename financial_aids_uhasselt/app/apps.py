f8c2506ae728c4cad41c1aa5fc872b3f84a357d4
abdullah10560 <abdullah.yalvac@student.uhasselt.be>
2025-03-28 18:35:15 +0100

    added missing yes no questions and more

--- financial_aids_uhasselt/app/apps.py
+++ financial_aids_uhasselt/app/apps.py
@@ -21,13 +21,19 @@ class AppConfig(AppConfig):
         ]
         
         #---gebruik deze line om vragen die nog in db waren te verwijderen---
-        #Question.objects.all().delete()
+        Question.objects.all().delete()
         basic_questions = [
             "Email address",
             "Faculty of student",
             "Amount of ECTS this year",
             "Domicile",
         ]
+        
+        yes_no_questions = [
+            "Are you staying in a student room?",
+            "Have you bought your laptop through the university?",
+        ]
+        
         for question_text in initial_questions:
             try:
                 Question.objects.get_or_create(question_text=question_text)  # Use the correct field name
@@ -39,3 +45,8 @@ class AppConfig(AppConfig):
                 Question.objects.get_or_create(question_text = question_text)
             except IntegrityError:
                 pass
+        for question_text in yes_no_questions:
+            try:
+                Question.objects.get_or_create(question_text=question_text)  # Use the correct field name
+            except IntegrityError:
+                pass  # Skip if the question already exists
